d5f27bf88878018ccb5034506fea98a0c9a147e7
Shantanu Raj <s@sraj.me>
2020-03-13 16:50:17 +0530

    Reset leaf state on cancelling request

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -14,6 +14,15 @@ export default function app(state = {}, action) {
         [key]: { state: "loaded", data }
       };
     }
+    case "API_REQUEST_CANCEL": {
+      const { key } = action.payload;
+      const leaf = state[key];
+      const loadingState = leaf.state === 'loading' ? 'initial' : leaf.state;
+      return {
+        ...state,
+        [key]: { ...leaf, state: loadingState }
+      };
+    }
     default:
       return state;
   }
